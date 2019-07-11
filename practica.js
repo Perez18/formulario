e38da143bf1ad325efb5bd1ba635f298d9f0cf8a
Perez18 <anthony.perez1@utp.ac.pa>
2019-07-11 12:34:22 -0500

    Commnit1
    practica con php

---
+++ practica.js
@@ -0,0 +1,50 @@
+/* 1. Pide la edad y si es mayor de 18 años indica que ya puede conducir.
+
+var a = prompt("indique su edad:");
+
+
+if (a >= 18)
+   {
+
+    console.log("Es mayor ")
+   }
+
+   else
+     {console.log("es menor")
+
+     }
+
+  */
+ 
+  
+  /*  Pide una nota (número). Muestra la calificación según la nota:
+
+0-3: Muy deficiente
+3-5: Insuficiente
+5-6: Suficiente
+6-7: Bien
+7-9: Notable
+9-10: Sobresaliente */
+
+/*var a = prompt("INGRESE NOTA:");
+
+var d = [0,1,2,3,4,5,6,7,8,9,10];
+
+var r=0;
+
+for ( r=0;r<=d.length;r++)
+{
+    if (d[r]<3){
+
+        console.log("Insuficiente")
+    }
+
+    else{
+
+        console.log("ivalido")
+    }
+  
+
+} 
+*/
+
